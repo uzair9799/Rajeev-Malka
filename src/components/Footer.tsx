ba15c9d84a6579ae526327ef742bfedaba576f44
sayed uzair <54931186+uzair9799@users.noreply.github.com>
2026-04-23 03:01:20 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="mt-auto border-t border-white/10 py-8 text-center opacity-50">
       <div className="container mx-auto px-4">
         <p className="font-sans text-[10px] uppercase tracking-[2px] text-cream">
-          Designed & Developed by Invitely Labs, Jaipur
+          Designed & Developed by <a href="https://www.instagram.com/sayed__uzairr"></a>Sayed Mohd Uzair</a></p>
         </p>
       </div>
     </footer>
